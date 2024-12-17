47ccb145f3c774f19aaa0fa2c884b5b3622e8f68
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-17 16:01:53 +0100

    create post with object

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const path = require('path');
 
 const port = 3000; // Imposta la porta su cui il server ascolta
 
+// Array di post
+const posts = [
+  {
+      titolo: "Introduzione a Express.js",
+      contenuto: "Express.js è un framework minimalista per Node.js.",
+      immagine: "",
+      tags: ["Node.js", "Express", "Backend"]
+  },
+  {
+      titolo: "JavaScript",
+      contenuto: "Funzionalità del linguaggio JavaScript.",
+      immagine: "",
+      tags: ["JavaScript", "JS", "Frontend"]
+  },
+  {
+      titolo: "API",
+      contenuto: "API con Express.js.",
+      immagine: "",
+      tags: ["API", "URL", "Express"]
+  },
+  {
+      titolo: "Database",
+      contenuto: "Database NoSQL",
+      immagine: "",
+      tags: ["Database", "Librerie", "NoSQL"]
+  },
+  {
+      titolo: "Bootstrap",
+      contenuto: "Guida all'utilizzo di Bootstrap",
+      immagine: "",
+      tags: ["Bootstrap", "CSS", "Card"]
+  }
+];
 
 // Rotta principale
 app.get('/', (req, res) => {
