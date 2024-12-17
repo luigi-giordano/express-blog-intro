d31eef73125b8abefd76628db27d2bb617394c22
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-17 15:40:46 +0100

    start server and test with thunderclient

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'); // Importazione di Express.js
+
+const app = express(); // Applicazione Express
+const port = 3000; // Imposta la porta su cui il server ascolta
+console.log('Ciao');
+
+// Rotta principale
+app.get('/', (req, res) => {
+    res.send('Server del mio blog'); // Risposta da inviare
+});
+
+// Avvio del server
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
+});
